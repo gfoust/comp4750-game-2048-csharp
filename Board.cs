d96fa0ed5af473d745e67e5a7b10ce2942759e15
Gabriel Foust <gfoust@harding.edu>
2025-08-17 15:25:04 -0500

    Sliding tiles

--- Board.cs
+++ Board.cs
@@ -1,59 +1,268 @@
 using Godot;
 using System;
+using System.Collections;
+using System.Collections.Generic;
+
 
 public partial class Board : Node2D
 {
-  Tile tile = null!;
+  public enum Dir { Up, Down, Left, Right };
+
+
+  const int GRID_SIZE = 4;
+  const float TILE_SIZE = 120;
+
+
   bool busy = false;
+  Tile?[,] grid = new Tile[GRID_SIZE, GRID_SIZE];
+  int emptyTiles = GRID_SIZE * GRID_SIZE;
+  Random random = new Random();
+  PackedScene tileScene = GD.Load<PackedScene>("res://tile.tscn");
+
+
+  public Tile? this[Vector2I pos]
+  {
+    get => grid[pos.X, pos.Y];
+    set => grid[pos.X, pos.Y] = value;
+  }
+
 
   public override void _Ready()
   {
-    tile = GetNode<Tile>("Tile");
+    addTileToGrid();
   }
+
 
 
   public override void _Input(InputEvent @event)
   {
     if (busy) return;
 
-    var newPos = tile.Position;
+    Dir? dir = null;
+
     if (@event.IsActionPressed("ui_left"))
     {
-      newPos.X = 0;
+      dir = Dir.Left;
     }
     else if (@event.IsActionPressed("ui_right"))
     {
-      newPos.X = 360;
+      dir = Dir.Right;
     }
     else if (@event.IsActionPressed("ui_up"))
     {
-      newPos.Y = 0;
+      dir = Dir.Up;
     }
     else if (@event.IsActionPressed("ui_down"))
     {
-      newPos.Y = 360;
+      dir = Dir.Down;
     }
 
-    if (newPos != tile.Position)
+    if (dir.HasValue && slide(dir.Value))
     {
-      moveTile(tile, newPos);
+      moveTiles();
     }
   }
 
 
-  private void moveTile(Tile tile, Vector2 newPosition)
+  private void moveTiles()
   {
     busy = true;
     Tween tween = CreateTween();
-    Console.WriteLine($"Tweening {tile.Name} from {tile.Position} to {newPosition}");
-    tween.TweenProperty(tile, "position", newPosition, 0.33333);
-    tween.Finished += onMoveFinished;
+    tween.SetParallel(true);
+    tween.SetTrans(Tween.TransitionType.Quart);
+    tween.SetEase(Tween.EaseType.Out);
+    for (int i = 0; i < GRID_SIZE; ++i)
+    {
+      for (int j = 0; j < GRID_SIZE; ++j)
+      {
+        if (grid[i, j] != null)
+        {
+          tween.TweenProperty(
+            grid[i, j],
+            "position",
+            new Vector2(i * TILE_SIZE, j * TILE_SIZE),
+            0.3333
+          );
+        }
+      }
+    }
+    tween.Finished += addTileToGrid;
   }
 
-  private void onMoveFinished()
+
+  private void addTileToGrid()
   {
     busy = false;
+    if (emptyTiles > 0)
+    {
+      int n = random.Next(emptyTiles);
+      for (int i = 0; i < GRID_SIZE; ++i)
+      {
+        for (int j = 0; j < GRID_SIZE; ++j)
+        {
+          if (grid[i, j] == null)
+          {
+            if (n == 0)
+            {
+              grid[i, j] = makeTile(i, j);
+              return;
+            }
+            --n;
+          }
+        }
+      }
+    }
   }
 
 
+  private Tile makeTile(int i, int j)
+  {
+    Tile tile = tileScene.Instantiate<Tile>();
+    tile.Position = new Vector2(i * TILE_SIZE, j * TILE_SIZE);
+    AddChild(tile);
+    --emptyTiles;
+    return tile;
+  }
+
+
+  private bool slide(Dir dir)
+  {
+    bool changed = false;  // Wait until you know why you need it
+
+    for (int i = 0; i < GRID_SIZE; ++i)
+    {
+      var dst = slice(dir, i);
+      dst.MoveNext();
+
+      var src = slice(dir, i);
+      while (src.MoveNext())
+      {
+        if (this[src.Current] != null)
+        {
+          if (src.Current != dst.Current)
+          {
+            this[dst.Current] = this[src.Current];
+            this[src.Current] = null;
+            changed = true;
+          }
+          dst.MoveNext();
+        }
+      }
+    }
+
+    return changed;
+  }
+
+
+  private static IEnumerator<Vector2I> slice(Dir dir, int along)
+  {
+    bool xAligned = dir == Dir.Left || dir == Dir.Right;
+
+    int begin, end, inc;
+    if (dir == Dir.Left || dir == Dir.Up)
+    {
+      begin = 0;
+      end = GRID_SIZE;
+      inc = 1;
+    }
+    else
+    {
+      begin = GRID_SIZE - 1;
+      end = -1;
+      inc = -1;
+    }
+
+    for (int current = begin; current != end; current += inc)
+    {
+      if (xAligned)
+      {
+        yield return new Vector2I(current, along);
+      }
+      else
+      {
+        yield return new Vector2I(along, current);
+      }
+    }
+  }
+
+
+  class SliceIterator : IEnumerator<Vector2I>
+  {
+    private Dir dir;
+    private int along;
+
+    private int begin;
+    private int end;
+    private bool xAligned;
+    private int inc;
+    private int current;
+
+
+    public SliceIterator(Dir dir, int along)
+    {
+      this.dir = dir;
+      this.along = along;
+
+      if (dir == Dir.Left || dir == Dir.Up)
+      {
+        begin = 0;
+        end = GRID_SIZE - 1;
+      }
+      else
+      {
+        begin = GRID_SIZE - 1;
+        end = 0;
+      }
+
+      xAligned = dir == Dir.Left || dir == Dir.Right;
+      inc = dir == Dir.Left || dir == Dir.Up ? -1 : 1;
+      current = begin;
+    }
+
+
+    Vector2I IEnumerator<Vector2I>.Current
+    {
+      get
+      {
+        if (xAligned)
+        {
+          return new Vector2I(current, along);
+        }
+        else
+        {
+          return new Vector2I(along, current);
+        }
+      }
+    }
+
+    object IEnumerator.Current
+    {
+      get
+      {
+        if (xAligned)
+        {
+          return new Vector2I(current, along);
+        }
+        else
+        {
+          return new Vector2I(along, current);
+        }
+      }
+    }
+
+    public void Dispose()
+    {
+    }
+
+    public bool MoveNext()
+    {
+      current += inc;
+      return current != end;
+    }
+
+    public void Reset()
+    {
+      current = begin - inc;
+    }
+  }
 }
--- notes.md
+++ notes.md
@@ -1,47 +1,42 @@
-# Stage 1
+# Stage 2
 
-1.  Start with Tile scene
-	- Node2D
-	- Add ColorRect
-	  + Set color
-	  + Set size to 119x119 (Layout, Transform)
-	  + Set offset to (1, 1)
-	  + This makes it 120x120, with border at top-left
-	- Add Label (under ColorRect)
-	  + Set text
-	  + Fill ColorRect
-	  + Set Horizontal and Vertical Align properties
-	  + Set font size = 48px
-2.  Make a tile script (C#!)
-	- Make power var (export, init 1)
-	- How can we get label?
-	- Create _ready method
-	  + Use `GetNode` to get label (compare to GD)
-	  + Set label text to `str(2**power)` (view)
-	  + Make the Color array
-		  * readonly vs static
-	  + Set color to `_colors[power]`
-3.  Make a board scene
-	- Add a ColorRect for background (481x481, #666)
-	  + Discuss: ColorRect could be the root node
-	- Add a tile (to the middle)
-	  + Discuss instantiate scene vs node
-	- We want tile to change when power changes
-	  + Make property
-	  + Make `_updateTile`
-		+ Make a guard for before ready
-	  + Make tile a tool
-	- Add script
-	  + Add tile variable; init in _Ready
-	  + Use _input method to move tile to edges
-	  + Add tween to animate the movement
-		  * IMPORTANT: must use GDScript name ("position") for tween
-	- Notice what happens when you change directions quickly
-	  + Make a `_busy` variable (false)
-			+ Make `_moveTile` that does the tween
-			  * Branches on input call `_moveTile`
-				* Set `_busy` to true
-			+ Make a `_onMoveTileDone` for when it is done
-			  * Set `_busy` to false
-				* Discuss lambdas
-			+ Make guard for `_Input`
+## Get rid of individual tile
+
+- Delete tile from board
+- This will break Board
+	+ In `_Input` change if branches to `pass`
+	+ In `moveTiles` remove `tween_property`
+
+## Add tiles dynamically
+
+- Make `GRID_SIZE` (int)
+- Make/initialize `grid` variable
+- Make `addTileToGrid`
+  + Save `busy = false` line for later
+  + Make `empty_tiles` variable
+  + Put in nth empty slot, where n is random
+  + Note: NOT `Tile.new()`; instead...
+- Make `make_tile`
+  + Make `tile_scene` variable
+  + Create / initialize tile
+- Add input for `ui_accept` to call `add_tile_to_grid`
+
+## Make tiles slide
+
+- Discuss abstraction, leading to iterator
+  + Discuss C# iterators (IEnumerator)
+  + Discuss our iterator interface (iterate over vectors)
+- Make `Dir` enum
+- Make `SliceItr` iterator
+  + Implement interface
+	+ Discuss yield
+- Make `slide` method
+  + Note: rearranging array has no visual effect
+- Revisit `moveTile` (=> `moveTiles`)
+  + Tween all tiles positions
+  + Don't cover tween properties yet
+- Debugging
+  - Add `addTileToGrid()` to `_Ready`
+  - Add `busy = false` to end of `addTileToGrid`
+  - Add `set_parallel`
+  - Add trans/ease
